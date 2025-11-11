c4e6daddc8852ddfc0fa0da5c5f74a637b6c5f9a
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2025-11-11 19:02:59 -0300

    ajustando identação

--- SVG.Infra/Migrations/202511112142098_Initial.cs
+++ SVG.Infra/Migrations/202511112142098_Initial.cs
@@ -1,28 +1,28 @@
 ﻿namespace SVG.Infra.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
-    public partial class Initial : DbMigration
+  using System;
+  using System.Data.Entity.Migrations;
+
+  public partial class Initial : DbMigration
+  {
+    public override void Up()
     {
-        public override void Up()
-        {
-            CreateTable(
-                "dbo.Operador",
-                c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Matricula = c.String(maxLength: 500, unicode: false),
-                        Nome = c.String(maxLength: 500, unicode: false),
-                        Sessao = c.String(maxLength: 500, unicode: false),
-                    })
-                .PrimaryKey(t => t.ID);
-            
-        }
-        
-        public override void Down()
-        {
-            DropTable("dbo.Operador");
-        }
+      CreateTable(
+          "dbo.Operador",
+          c => new
+          {
+            ID = c.Int(nullable: false, identity: true),
+            Matricula = c.String(maxLength: 500, unicode: false),
+            Nome = c.String(maxLength: 500, unicode: false),
+            Sessao = c.String(maxLength: 500, unicode: false),
+          })
+          .PrimaryKey(t => t.ID);
+
     }
+
+    public override void Down()
+    {
+      DropTable("dbo.Operador");
+    }
+  }
 }
